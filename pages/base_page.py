fb659cdce4c2dd989701fc105c86f7a2e5cb2a8f
Dima <mandarin666.dr@gmail.com>
2025-05-18 20:15:45 +0300

    update файлов под отлов исключений

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
@@ -5,7 +6,14 @@ class BasePage():
         self.browser = browser
         self.url = url
 
+
     def open(self):
         self.browser.get(self.url)
 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
