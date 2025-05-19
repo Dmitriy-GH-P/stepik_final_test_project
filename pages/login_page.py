f8a5ca19bb9ae266d6ea8c8c2451654d65a6d981
Dima <mandarin666.dr@gmail.com>
2025-05-19 19:07:27 +0300

    добавлены новые локаторы и методы для login page

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from conftest import browser
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self, browser):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in browser.current_url , "incorrect url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form is not found"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is not found"
